9a1c460feeae6c697029bb9ca5fe4bdfb97aafc5
Qihang Yang <442621753@qq.com>
2018-08-25 16:30:04 +0800

    coinex market api implement

--- trx/market.go
+++ trx/market.go
@@ -8,7 +8,10 @@ type CoinexMarket struct{}
 var apiMap = map[string]string{
 	"marketList": "https://api.coinex.com/v1/market/list",
 	"marketTicker": "https://api.coinex.com/v1/market/ticker",
-	"marketTickerAll": "https://api.coinex.com/v1/market/ticker/all"}
+	"marketTickerAll": "https://api.coinex.com/v1/market/ticker/all",
+	"marketDepth": "https://api.coinex.com/v1/market/depth",
+	"trxData": "https://api.coinex.com/v1/market/deals",
+	"kLine": "https://api.coinex.com/v1/market/kline"}
 
 
 func(m *CoinexMarket) GetMarketList() *[]string{
@@ -70,4 +73,88 @@ func(m *CoinexMarket) GetMarketTickerAll() *map[string]map[string]string{
 	}
 
 
-func(m *CoinexMarket) 
+func(m *CoinexMarket) GetMarketDepth(marketType string, merge string, limit string) *map[string][][]string {
+	var marketDepth = make(map[string][][]string)
+
+	newRequest, _ := http.NewRequest("GET", apiMap["marketDepth"], nil)
+
+	q := newRequest.URL.Query()
+	q.Add("market", marketType)
+	q.Add("merge", merge)
+	q.Add("limit", limit)
+	newRequest.URL.RawQuery = q.Encode()
+
+	c := CoinexRequest{req: newRequest}
+	c.SetHeader()
+	resp := c.DoRequest()
+
+	jsonparser.ObjectEach(resp, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error{
+		var valueSlice [][]string
+		if string(key) == "asks" || string(key) == "bids"{
+			jsonparser.ArrayEach(value, func(value []byte, dataType jsonparser.ValueType, offset int, err error){
+				var tempContainer []string
+				jsonparser.ArrayEach(value, func(value []byte, dataType jsonparser.ValueType, offset int, err error){
+					tempContainer = append(tempContainer, string(value))
+				})
+				valueSlice = append(valueSlice, tempContainer)
+			})
+		}
+		if valueSlice != nil{
+			marketDepth[string(key)] = valueSlice
+		}
+		return nil
+	}, "data")
+	return &marketDepth
+}
+
+func(m *CoinexMarket) GetTrxData(marketType string, last_id string) *[]map[string]string {
+	var trxData []map[string]string
+
+	newRequest, _ := http.NewRequest("GET", apiMap["trxData"], nil)
+
+	q := newRequest.URL.Query()
+	q.Add("market", marketType)
+	q.Add("last_id", last_id)
+	newRequest.URL.RawQuery = q.Encode()
+
+	c := CoinexRequest{req: newRequest}
+	c.SetHeader()
+	resp := c.DoRequest()
+
+	jsonparser.ArrayEach(resp, func(value []byte, dataType jsonparser.ValueType, offset int, err error){
+		var valueMap = make(map[string]string)
+
+		jsonparser.ObjectEach(value ,func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error{
+			valueMap[string(key)] = string(value)
+			return nil
+		})
+		trxData = append(trxData, valueMap)
+
+	}, "data")
+	return &trxData
+}
+
+func(m *CoinexMarket) GetKLine(marketType, limit, kType string) *[][]string{
+	var kLine [][]string
+
+	newRequest, _ := http.NewRequest("GET", apiMap["kLine"], nil)
+
+	q := newRequest.URL.Query()
+	q.Add("market", marketType)
+	q.Add("limit", limit)
+	q.Add("type", kType)
+	newRequest.URL.RawQuery = q.Encode()
+
+	c := CoinexRequest{req: newRequest}
+	c.SetHeader()
+	resp := c.DoRequest()
+
+	jsonparser.ArrayEach(resp, func(value []byte, dataType jsonparser.ValueType, offset int, err error){
+		var valueSlice []string
+	jsonparser.ArrayEach(value, func(childValue []byte, dataType jsonparser.ValueType, offset int, err error){
+		valueSlice = append(valueSlice, string(childValue))
+	})
+		kLine = append(kLine, valueSlice)
+	}, "data")
+	return &kLine
+}
--- trx/market_test.go
+++ trx/market_test.go
@@ -26,3 +26,24 @@ func TestGetMarketTickerAll(t *testing.T) {
 	resp := coinexMarket.GetMarketTickerAll()
 	fmt.Println(*resp)
 }
+
+func TestGetMarketDepth(t *testing.T) {
+	coinMarket := CoinexMarket{}
+
+	resp := coinMarket.GetMarketDepth("BTCBCH", "0", "5")
+	fmt.Println(*resp)
+}
+
+func TestGetTrxData(t *testing.T) {
+	coinMarket := CoinexMarket{}
+
+	resp := coinMarket.GetTrxData("BTCBCH", "0")
+	fmt.Println(*resp)
+}
+
+func TestKline(t *testing.T) {
+	coinMarket := CoinexMarket{}
+
+	resp := coinMarket.GetKLine("BTCBCH", "10", "1min")
+	fmt.Println(*resp)
+}
